3408660fbf2446f674817448d41ca5ccda6262cf
mark-hss <he62935@health.wa.gov.au>
2026-06-15 16:14:53 +0800

    cli utils

---
+++ ruby/cli/count.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+#top 10 words by count
+file=ARGV[0]
+
+words = Hash.new(0)
+
+File.open(file, "r").each_line do |line|
+  line.scan(/\b\w+\b/) {|i| words[i] +=1}
+end
+
+sorted = words.sort_by {|a| a[1]}
+
+temp = sorted.length
+
+10.times do
+  temp -= 1
+  puts "\"#{sorted[temp][0]}\" has #{sorted[temp][1]} occurances"
+end
